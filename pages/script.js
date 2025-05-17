c1144513347eac86de9d521dc80ca36069d97995
1phantm1 <pietrodavila37@gmail.com>
2025-05-17 17:02:13 -0300

    Update

--- pages/script.js
+++
@@ -1,6 +0,0 @@
-let holiday = document.getElementsByClassName("td.holiday");
-let holidayTitle = document.getElementsByClassName("div.holiday-title")
-
-holiday.addEventListener("mouseover", (event) => {
-    event.target.style.display = "block"
-})
